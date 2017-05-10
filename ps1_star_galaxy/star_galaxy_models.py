27c76c7f6c4cf7902ee39df85070e4f32e32ae0e
Adam Miller <amiller.astro@gmail.com>
2017-05-10 16:48:47 -0500

    Updated doc strings and case handling for sources where no PSF model exists from PS1.

--- ps1_star_galaxy/star_galaxy_models.py
+++ ps1_star_galaxy/star_galaxy_models.py
@@ -17,9 +17,33 @@ class RandomForestModel:
     def create_training_set(self, ps1_fits_file, features = None, 
                             training_set = False, target_name = 'class', 
                             random_state = None):
+        
         """
         Generate training set attribute from fits file
         
+        Starting from a fits file that contains output from the PanSTARRS1 
+        DR1 database, specifically the StackObjectAttributes table, this 
+        function reads the file and generates features for use in scikit-learn
+        machine learning models. The features are stored as a pandas DataFrame
+        attribute of the class.
+        
+        Note - the following columns from the PS1 StackObjectAttributes are 
+        assumed to be in ps1_fits_file (if they are missing the code will 
+        break):
+        
+            gpsfMajorFWHM, gpsfMinorFWHM,
+            gpsfCore, gpsfLikelihood, gpsfChiSq, gExtNSigma,
+            gmomentXX, gmomentXY, gmomentYY, gmomentR1, gmomentRH, gKronRad,
+            gPSFFlux, gKronFlux
+        
+        as well as similar entries in the same row for rizy filters.
+                            
+        Furthermore - for some sources PS1 was unable to measure some of 
+        these features. That is typically indicated by a value of -999. This 
+        code handles missing data by converting the respective features to a 
+        value of 1e6. Note - sources with psfMajorFWHM = 0 do not necessarily
+        indicate missing data, this needs to be investigated further.
+                            
         Parameters
         ----------
         ps1_fits_file : file
@@ -69,7 +93,6 @@ class RandomForestModel:
                    'ypsfCore', 'ypsfLikelihood', 'ypsfChiSq', 'yExtNSigma']
         self.X_df_ = raw_ps1_df[no_norm]
         
-        
         normed_feat_list = ['momentXX', 'momentXY', 'momentYY', 
                             'momentR1', 'KronRad', 'momentRH']
         
@@ -83,7 +106,8 @@ class RandomForestModel:
             feat_norm_terms = np.array([filt_seeing**2, filt_seeing**2, 
                                         filt_seeing**2, filt_seeing, 
                                         filt_seeing, filt_seeing**0.5]).T
-            feat_norm_terms[feat_norm_terms == 0] = -0.0    
+            no_psf_model = np.where(filt_seeing <= 0)
+            feat_norm_terms[no_psf_model] = -0.0    
                         
             ff_list = [filt + feat for feat in normed_feat_list]
             ff_norm_list = [filt + feat + "norm" for feat in normed_feat_list]
@@ -91,22 +115,27 @@ class RandomForestModel:
             ff_kwargs = {x : raw_ps1_df[y]/feat_norm_terms[:,z] 
                 for x, y, z, in zip(ff_norm_list, ff_list, range(6))}
             
-            self.X_df_.assign(**ff_kwargs)
+            self.X_df_ = self.X_df_.assign(**ff_kwargs)
         
             psf_det = np.array(raw_ps1_df[filt + 'PSFFlux'] > 0).astype(int)
             kron_det = np.array(raw_ps1_df[filt + 'KronFlux'] > 0).astype(int)
             
             psf_det_flux = psf_det*raw_ps1_df[filt + 'PSFFlux'].values
             kron_det_flux = kron_det*raw_ps1_df[filt + 'KronFlux'].values
+                        
+            filt_flux_ratio = np.divide(psf_det_flux, kron_det_flux, 
+                                        out = np.zeros_like(psf_det_flux), 
+                                        where = kron_det_flux != 0)
+            f_kwargs = {filt + 'psfKronRatio' : filt_flux_ratio}
+            self.X_df_ = self.X_df_.assign(**f_kwargs)
             
             psf_flux_w += psf_det_flux
             kron_flux_w += kron_det_flux
             
-            f_kwargs = {filt + 'psfKronRatio' : np.nan_to_num(psf_det_flux / 
-                                                              kron_det_flux)}
-            self.X_df_.assign(**f_kwargs)
-            
-        w_kwargs = {'wpsfKronRatio' : np.nan_to_num(psf_flux_w/kron_flux_w)}
-        self.X_df_.assign(**w_kwargs)
-            
+        w_flux_ratio = np.divide(psf_flux_w, kron_flux_w, 
+                                 out = np.zeros_like(psf_flux_w), 
+                                 where = kron_flux_w != 0)
+        w_kwargs = {'wpsfKronRatio' : w_flux_ratio}
+        self.X_df_ = self.X_df_.assign(**w_kwargs)
+        self.X_df_.replace([-np.inf, np.inf], 1e6, inplace = True)
             
